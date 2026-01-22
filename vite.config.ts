19921907458c0d20ccbb000415faee629c9c42c2
the-real-i9 <oluwarinolasam@gmail.com>
2026-01-22 21:31:18 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/i9ine/",
+  base: "./",
+  build: {
+    outDir: "i9ine",
+  },
 })
